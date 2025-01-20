f78542192128dd595c332582059d30c206d7c3df
Андрей <andre919andre@mail.ru>
2025-01-21 01:15:52 +0300

    first commit

---
+++ js/header/burger/burger.js
@@ -0,0 +1,20 @@
+// бургер тестовый вариант
+
+// const button = document.querySelector('.button');
+// const menu = document.querySelector('.nav');
+// const menuLink = document.querySelectorAll('.menu__link')
+// button.addEventListener('click', () => {
+//   button.classList.toggle('active')
+//   if (button.classList.contains('active')) {
+//     button.setAttribute('aria-expanded', 'true')
+//     menu.setAttribute('aria-hidden', 'false')
+//     menuLink.forEach(link => link.setAttribute('tabindex', '0'))
+//   } else {
+//     button.setAttribute('aria-expanded', 'false')
+//     menu.setAttribute('aria-hidden', 'true')
+//     menuLink.forEach(link => link.setAttribute('tabindex', '-1 '))
+//   }
+
+//   button.setAttribute('aria-expanded', button.classList.contains('active') ? 'true' : 'false')
+// });
+
